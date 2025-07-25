fca483c1b2788413c7984fae0ddab5638ecb4128
poetri598 <poetri598@example.com>
2025-07-25 22:09:05 +0700

    25/07/2025:10:09:PM

--- src/utils/useIsAuthenticated.ts
+++
@@ -1,8 +0,0 @@
-// utils/useIsAuthenticated.ts
-import { useSession } from "@/utils/useSession";
-import { isAuthenticated as check } from "@/services/auth";
-
-export function useIsAuthenticated(): boolean {
-  const user = useSession();
-  return check(user);
-}
